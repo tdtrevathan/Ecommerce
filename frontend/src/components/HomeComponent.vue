4cf26e3436bca574779dbc683506ab06488e97f8
tdtrevathan <trevathantd@gmail.com>
2024-06-27 10:52:15 -0500

    renamed frontend to get files to persist

---
+++ frontend/src/components/HomeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Tim's Website</h1>
+  <div class="container">
+    <CarouselComponent></CarouselComponent>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CarouselComponent from '@/components/CarouselComponent.vue'
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: grey;
+}
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <HomeComponent/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HomeComponent from '@/components/HomeComponent.vue';
+</script>
