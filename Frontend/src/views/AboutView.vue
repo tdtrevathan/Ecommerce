d6f661df2b1b33b950e26fa77ee47ce77484572a
tdtrevathan <trevathantd@gmail.com>
2024-06-27 15:01:52 -0500

    changed stripe modal method

--- Frontend/src/views/AboutView.vue
+++ Frontend/src/views/AboutView.vue
@@ -1,132 +1,152 @@
 <template>
-    <div class="about">
-      <div class="modal">
-        <div class="modal-content">
-          <form @submit.prevent="handleSubmit">
-            <label for="card-element">Credit or debit card</label>
-            <div id="card-element"></div>
-            <button type="submit">Pay</button>
-          </form>
-        </div>
+  <div class="about">
+    <div class="modal">
+      <div class="modal-content">
+        <form id="payment-form" @submit.prevent='handleSubmit'>
+          <div id="payment-element">
+            <!--Stripe.js injects the Payment Element-->
+          </div>
+          <button id="submit">
+            <div class="spinner hidden" id="spinner"></div>
+            <span id="button-text">Pay now</span>
+          </button>
+          <div id="payment-message" class="hidden"></div>
+        </form>
       </div>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { StripeCardElement, loadStripe } from '@stripe/stripe-js';
-  import { onMounted, ref } from 'vue';
-  import axios from 'axios'
-  
-  //TODO turn key into secret
-  let stripePromise:any = null;
-  const cardElement = ref<StripeCardElement | null>(null)
-  const clientSecret = ref(''
+  </div>
+</template>
 
-  )
-  onMounted(async () => {
-    try {
-      stripePromise = loadStripe(await getPublicKey());
-      const stripe = await stripePromise;
-      if (!stripe) {
-        console.log("Stripe failed")
-        return
-      }
-      const elements = stripe.elements();
-      cardElement.value = elements.create('card');
-      if (cardElement.value) {
-        cardElement.value.mount('#card-element');
+<script setup lang="ts">
+import { StripeCardElement, StripeElements, loadStripe } from '@stripe/stripe-js';
+import { onMounted, ref } from 'vue';
+import axios from 'axios'
 
-        const response = await axios.post('http://localhost:5000/create-payment-intent', { amount: 100 }); // Amount in cents
-        clientSecret.value = response.data.clientSecret;
-      }
-      else {
-        console.log('failed to mount')
-      }
+//TODO turn key into secret
+let stripePromise: any = null;
+let stripe: any = null;
+
+const clientSecret = ref('')
+const elements = ref<StripeElements | null>(null);
+
+onMounted(async () => {
+  try {
+    stripePromise = loadStripe(await getPublicKey());
+    stripe = await stripePromise;
+    if (!stripe) {
+      console.log("Stripe failed")
+      return
     }
-    catch (error) {
-      console.log(error)
-    }
-  });
 
-  const handleSubmit = async () => {
-    const errorMessage = ref('');
-    const stripe = await stripePromise;
-    
-    if (cardElement.value) {
+    const response = await axios.post('http://localhost:5000/create-payment-intent', { amount: 100 }); // Amount in cents
+    clientSecret.value = response.data.clientSecret;
 
-      const { error, paymentIntent } =
-        await stripe.confirmCardPayment(clientSecret.value, {
-          payment_method: {
-            card: cardElement.value,
+    mountModal(clientSecret.value)
+
+  }
+  catch (error) {
+    console.log(error)
+  }
+});
+
+const mountModal = (clientSecret: string) => {
+  const appearance = {
+    theme: 'stripe',
+  };
+
+  elements.value = stripe.elements({ appearance, clientSecret });
+
+  if(elements.value){
+
+    const paymentElement = elements.value.create("payment");
+    paymentElement.mount("#payment-element");
+  
+  }
+  else {
+    console.log('failed to mount')
+  }
+}
+
+const handleSubmit = async () => {
+  const errorMessage = ref('');
+  const stripe = await stripePromise;
+
+  try{
+    const { error, paymentIntent } =
+      await stripe.confirmPayment({
+        elements: elements.value,
+        confirmParams: {
+            // Omit return_url if you don't want a redirect yet
+            // return_url: 'https://your-return-url.com',
           },
-        });
-      
-      if (error) {
-        errorMessage.value = error.message ? error.message : 'message undefined';
-      } 
-      else if (paymentIntent.status === 'succeeded') {
-        alert('Payment succeeded!');
-        //closeModal();
-      }
-    }
-    else{
-      alert('something went wrong')
-    }
-  }
+          redirect: 'if_required',
+      });
 
-  const getPublicKey = async () => {
-    try{
-      const param = 'PublicStripeToken'
-      const response = await axios.get(`http://localhost:5000/secret/${param}`);
-      console.log("RESPONSE", response)
-      console.log("RESPONSE DATA", response.data)
-      return response.data;
+    if (error) {
+      errorMessage.value = error.message ? error.message : 'message undefined';
     }
-    catch(err){
-      console.log(err);
+    else if (paymentIntent.status === 'succeeded') {
+      alert('Payment succeeded!');
+      //closeModal();
     }
   }
-  </script>
-  
-  <style scoped>
+  catch(err){
+    console.log(err)
+  }
+}
 
-  #card-element{
-    width: 400px;
+const getPublicKey = async () => {
+  try {
+    const param = 'PublicStripeToken'
+    const response = await axios.get(`http://localhost:5000/secret/${param}`);
+    return response.data;
   }
+  catch (err) {
+    console.log(err);
+  }
+}
+</script>
 
-  .about {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-color: rgba(0, 0, 0, 0.5);
-  }
-  
-  .modal-content {
-    background: white;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  }
-  
-  .modal {
-    max-width: 15000px;
-    width: 100%;
-  }
-  
-  button {
-    margin-top: 20px;
-    padding: 10px 20px;
-    background-color: #6772e5;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    font-size: 16px;
-  }
-  
-  button:hover {
-    background-color: #5469d4;
-  }
-  </style>
-  
+<style scoped>
+.about {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  display: inline-block;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 200px;
+  height: 450px;
+}
+
+.modal {
+  max-width: 15000px;
+  width: 100%;
+  height: 400px;
+}
+
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  background-color: #6772e5;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+}
+
+button:hover {
+  background-color: #5469d4;
+}
+</style>
